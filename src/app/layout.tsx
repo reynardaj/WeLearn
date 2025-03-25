8f7fc9880f26688b62d5721e99c66ceca1cd7f6a
jason32456 <jasonchristian2112@gmail.com>
2025-03-25 21:09:22 +0700

    user button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider, SignedIn, SignedOut, SignIn } from "@clerk/nextjs";
+import { ClerkProvider, SignedIn, SignedOut, SignIn, UserButton } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +29,10 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased flex items-center justify-center min-h-screen`}
         >
+          <header className="flex justify-between">
+            {/* <h1>WeLearn</h1> */}
+            <UserButton showName />
+          </header>
           <main>
             <SignedOut>
               <SignIn routing="hash" />
