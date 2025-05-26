d07f4963b16b06fe3ea80190361053b9c860fbe2
Kent <kentel3rt@gmail.com>
2025-05-26 22:37:06 +0700

    tes

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -26,7 +26,7 @@ export const Button: React.FC<ButtonProps> = ({
     textClass = "text-white";
   } else if (variant == "secondary") {
     variantClass = "bg-[#F4B660] text-black hover:bg-[#F4B660]";
-    textClass = "text-white";
+    textClass = "text-black";
   } else if (variant == "ghost") {
     variantClass = "bg-transparent text-gray-700 hover:underline";
     textClass = "text-black";
